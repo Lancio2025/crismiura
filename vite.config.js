fef017de014b1c5db7b661cbac0e106c82c01639
Lancio2025 <contato@lancio.com.br>
2025-08-14 15:59:48 -0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,23 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: './',
   build: {
-    outDir: 'dist',
-    assetsDir: 'assets',
-    rollupOptions: {
-      output: {
-        chunkFileNames: 'assets/js/[name]-[hash].js',
-        entryFileNames: 'assets/js/[name]-[hash].js',
-        assetFileNames: 'assets/[ext]/[name]-[hash].[ext]',
-      }
-    }
-  },
-  server: {
-    port: 3000,
-    open: true
-  },
-  preview: {
-    port: 4173
+    outDir: 'dist'
   }
 })
